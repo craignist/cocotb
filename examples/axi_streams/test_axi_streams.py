0f3e59625612dfaddd6e4416f87b7ec1c9ec4c73
nelson <nelson@nist.gov>
2018-11-29 17:56:49 -0700

    Add more examples for vector generation in test_axis_streams examples

--- examples/axi_streams/test_axi_streams.py
+++ examples/axi_streams/test_axi_streams.py
@@ -25,11 +25,8 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. '''
 
-import random
 import logging
-
 import cocotb
-import numpy as np
 
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, Event
@@ -43,47 +40,53 @@ from FixedPoint import FXfamily
 
 
 # Data generators
-from cocotb.generators.bit import (wave, intermittent_single_cycles,
-                                   random_50_percent)
+from cocotb.generators.bit import (wave, intermittent_single_cycles, random_50_percent)
+from cocotb.generators.numeric import (random_data_gen, add_gen, sine_data_gen)
+from cocotb.generators.numeric import (ramp_packet_gen, data_packet_gen)
 
 
 class EndianSwapperTB(object):
-
-    def __init__(self, dut, clk, reset, latency, debug=False):
+    def __init__(self, dut, clk, reset, latency, in_type=int, out_type=int, debug=False):
         self.dut = dut
         self.aclk = clk
         self.aresetn = reset
-        self.axis_tdata_size = dut.AXIS_TDATA_WIDTH
+
+        # Read module parameters from DUT verilog file
+        self.axis_tdata_size = int(dut.AXIS_TDATA_WIDTH)
+        self.integer_bits = 2
         self.latch_type = dut.REG_TYPE
-        fptype_in = FXfamily(6, 2)
-        fptype_out = FXfamily(6, 2)
-        self.stream_in = AxisMaster(dut, "s_axis", clk, dtype=fptype_in)
+
+        # Define AXIS drivers and monitors
+        self.m_axis = AxisMaster(dut, "s_axis", clk, dtype=in_type)
         self.backpressure = BitDriver(self.dut.m_axis_tready, clk)
+
+        # Event is needed to delay posting of output data, so it arrives at the scoreboard
+        # after the modeled input data.
         if self.latch_type == 0:
             self.event = Event()
         else:
             self.event = None
-        self.stream_out = AxisSlave(dut, "m_axis", clk, dtype=fptype_out, event=self.event)
+        self.s_axis = AxisSlave(dut, "m_axis", clk, dtype=out_type, event=self.event)
 
-        # Reconstruct the input transactions from the pins
+        # Reconstruct the input transactions from what the DUT accepts
         # and send them to our 'model'
-        self.stream_in_recovered = AxisSlave(dut, "s_axis", clk, callback=self.model, dtype=fptype_in)
+        self.m_axis_sent = AxisSlave(dut, "s_axis", clk, callback=self.model, dtype=in_type)
 
-        # Create a scoreboard on the stream_out bus
+        # Create a scoreboard on the.s_axis bus
         self.pkts_sent = 0
         self.expected_output = []
         self.scoreboard = Scoreboard(dut)
-        self.scoreboard.add_interface(self.stream_out, self.expected_output, strict_type=True)
+        self.scoreboard.add_interface(self.s_axis, self.expected_output, strict_type=True)
 
         # Set verbosity on our various interfaces
         level = logging.DEBUG if debug else logging.WARNING
-        self.stream_in.log.setLevel(level)
-        self.stream_in_recovered.log.setLevel(level)
+        self.m_axis.log.setLevel(level)
+        self.m_axis_sent.log.setLevel(level)
 
     def model(self, transaction):
         """Model the DUT based on the input transaction"""
         self.dut._log.info("Sent a packet of %d words" % len(transaction))
-        # trans = [FXnum(float(x),self.stream_out.dtype) for x in transaction]
+        # trans = [FXnum(float(x),self.s_axis.dtype) for x in transaction]
         trans = transaction
         self.expected_output.append(trans)
         self.pkts_sent += 1
@@ -95,37 +98,51 @@ class EndianSwapperTB(object):
     def reset(self):
         self.dut._log.debug("Resetting DUT")
         self.aresetn <= 0
-        self.stream_in.bus.tvalid <= 0
-        self.stream_in.bus.tlast <= 0
-        self.stream_in.bus.tdata <= 0
-        self.stream_out.bus.tready <= 0
+        self.m_axis.bus.tvalid <= 0
+        self.m_axis.bus.tlast <= 0
+        self.m_axis.bus.tdata <= 0
+        self.s_axis.bus.tready <= 0
         yield RisingEdge(self.aclk)
         yield RisingEdge(self.aclk)
         yield RisingEdge(self.aclk)
         self.aresetn <= 1
-        self.stream_out.bus.tready <= 1
+        self.s_axis.bus.tready <= 1
         self.dut._log.debug("Out of reset")
 
 
 @cocotb.coroutine
-def run_test(dut, data_in=None, idle_inserter=None,
+def run_test(dut, data_config=None, idle_inserter=None,
              backpressure_inserter=None, pipeline=10):
+
     clk = dut.aclk
     cocotb.fork(Clock(clk, 5000, 'ps').start())
-    tb = EndianSwapperTB(dut, clk, dut.aresetn, pipeline)
+
+    # Read module parameters from DUT verilog file
+    tdata_size = int(dut.AXIS_TDATA_WIDTH)
+    integer_bits = 1
+
+    if data_config['dtype'] == float:
+        # Define data types
+        type_in = FXfamily(tdata_size - integer_bits, integer_bits)
+        type_out = FXfamily(tdata_size - integer_bits, integer_bits)
+    else:
+        type_in = int
+        type_out = int
+
+    tb = EndianSwapperTB(dut, clk, dut.aresetn, pipeline, type_in, type_out)
 
     yield tb.reset()
 
     # Start off any optional coroutines
     if idle_inserter is not None:
-        tb.stream_in.set_valid_generator(idle_inserter())
+        tb.m_axis.set_valid_generator(idle_inserter())
     if backpressure_inserter is not None:
         tb.backpressure.start(backpressure_inserter())
 
     npkts = 0
     # Send in the packets
-    for transaction in data_in():
-        yield tb.stream_in.send(transaction)
+    for transaction in data_config['packet_generator'](data_config):
+        yield tb.m_axis.send(transaction)
         npkts += 1
 
     # Drop input tvalid
@@ -145,36 +162,56 @@ def run_test(dut, data_in=None, idle_inserter=None,
     raise tb.scoreboard.result
 
 
-'''
-def random_float(lower=-1.0, upper=1.0):
-    while True:
-        yield np.random.uniform(lower,upper)
+config_ramp = {'packet_generator': ramp_packet_gen,
+              'lower': 0,
+              'upper': 128,
+              'packet_size': 128,
+              'n_packets': 6,
+              'dtype': int}
 
-def random_int(lower=0, upper=256):
-    return random.randrange(lower,upper))
-'''
+config_rand= {'packet_generator': data_packet_gen,
+              'data_generator': random_data_gen,
+              'lower': -128,
+              'upper': 128,
+              'packet_size': 1024,
+              'n_packets': 4,
+              'dtype': int}
 
+config_sin = {'packet_generator': data_packet_gen,
+              'data_generator': sine_data_gen,
+              'lower': -1.0,
+              'upper': 0.99999,
+              'packet_size': 1024,
+              'n_packets': 4,
+              'dt': 8e-9,
+              'freq': 488281.25,
+              'dtype': float}
 
-def random_floats(num, lower=-1.0, upper=1.0):
-    return [np.random.uniform(lower, upper) for _ in range(num)]
+factory1 = TestFactory(run_test)
+factory1.add_option("data_config", [config_ramp, config_rand, config_sin])
+factory1.add_option("idle_inserter", [None, wave, intermittent_single_cycles, random_50_percent])
+factory1.add_option("backpressure_inserter", [None, wave, intermittent_single_cycles, random_50_percent])
+factory1.generate_tests("Idle & Backpressure Tests_")
 
+config_noise  = {'data_generator': random_data_gen,
+                 'lower': -0.5,
+                 'upper': 0.5,
+                 'dtype': float}
 
-def random_ints(num, lower=0, upper=9):
-    return [random.randrange(lower, upper) for _ in range(num)]
+config_signal = { 'data_generator': sine_data_gen,
+                  'lower': -0.5,
+                  'upper': 0.5,
+                  'dt': 8e-9,
+                  'freq': 488281.25,
+                  'dtype': float}
 
+config_add = {'packet_generator': data_packet_gen,
+              'data_generator': add_gen,
+              'sub_configs': [config_noise, config_signal],
+              'packet_size': 1024,
+              'n_packets': 4,
+              'dtype': float}
 
-def random_packet_sizes(data_generator=None, config_gen=None):
-    """random string data of a random length"""
-    for i in range(5):
-        pkt_size = random.randint(20, 20)
-        yield random_floats(pkt_size, -1.0, 1.0)
-
-
-factory = TestFactory(run_test)
-factory.add_option("data_in",
-                   [random_packet_sizes])
-factory.add_option("idle_inserter",
-                   [None, wave, intermittent_single_cycles, random_50_percent])
-factory.add_option("backpressure_inserter",
-                   [None, wave, intermittent_single_cycles, random_50_percent])
-factory.generate_tests("A_")
+factory2 = TestFactory(run_test)
+factory2.add_option("data_config", [config_add])
+factory2.generate_tests("Signal Test_")
